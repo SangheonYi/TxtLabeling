f23d99a5a61664366fa9844307ae19dd5081d72a
SangheonYi <sayidev42@fasoo.com>
2022-04-19 11:13:32 +0900

    refactor: Refarctor color theme

--- label_domain_radio.py
+++ label_domain_radio.py
@@ -6,25 +6,27 @@ def rgb_to_hex(r, g, b):
     return '#' + hex(r)[2:].zfill(2) + hex(g)[2:].zfill(2) + hex(b)[2:].zfill(2)
 
 def init_radio(txt_labeling):
-    half_size = len(txt_labeling.files[LABEL].label_domain) // 4 - 1
+    half_size = len(txt_labeling.files[LABEL].label_domain) // 3 - 1
     for i, label in enumerate(txt_labeling.files[LABEL].label_domain):
         radio_button = tk.Radiobutton(txt_labeling.app, text=label, value=label, variable=txt_labeling.picked_label)
         if not (label == 'O' or label == 'UNK'):
-            red, green, blue = 240, 240, 120
+            red, green, blue = 255, 255, 255
             offset_base = 255 // half_size
-            offset = i // 4
-            if i % 4 > 1:
-                green = offset * offset_base
-                if label[-1] == 'I':
-                    red -= 30
-                    blue -= 30
-                if green > 127:
-                    red //= 2
-            else:
+            offset = i // 3
+            i_mod = i % 6
+            if  i_mod < 2:
                 red = offset * offset_base
-                blue = (half_size - offset) * offset_base
-                if label[-1] == 'I':
-                    green -= 30
+                green = 0
+            elif 1 < i_mod < 4:
+                green = offset * offset_base
+                blue = 0
+            else:
+                red = 0
+                blue = offset * offset_base
+            if label[-1] == 'I':
+                red = red // 2 + 30
+                green = green // 2 + 30
+                blue = blue // 2 + 30
             txt_labeling.color_dict[label] = rgb_to_hex(red, green, blue)
             radio_button.config(bg=txt_labeling.color_dict[label])
         radio_button.grid(row=LABEL_DOMAIN_ROW + (i // 10),column=i % 10)
